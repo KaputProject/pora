125fa7695db155d29944e2bceb79c0479a2336d3
kuderluka <luka.kuder@gmail.com>
2025-12-03 17:39:12 +0100

    KAPUT-10: Dodan Gumb za dovoljenja

--- app/src/main/java/si/um/feri/kaput/SettingsActivity.kt
+++ app/src/main/java/si/um/feri/kaput/SettingsActivity.kt
@@ -1,17 +1,24 @@
 package si.um.feri.kaput
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import si.um.feri.kaput.databinding.ActivityMainBinding
 
 class SettingsActivity : AppCompatActivity() {
+    lateinit var app: MyApplication
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_settings)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        app = application as MyApplication
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
