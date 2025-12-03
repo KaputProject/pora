032eeba08c9dfdb3279e0ad508f7a43bd1c7a65a
kuderluka <luka.kuder@gmail.com>
2025-12-03 20:54:13 +0100

    KAPUT-10: Dodan gumb za dovoljenja

--- app/src/main/java/si/um/feri/kaput/SettingsActivity.kt
+++ app/src/main/java/si/um/feri/kaput/SettingsActivity.kt
@@ -1,16 +1,19 @@
 package si.um.feri.kaput
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import si.um.feri.kaput.databinding.ActivityMainBinding
+import si.um.feri.kaput.databinding.ActivitySettingsBinding
+import si.um.feri.kaput.utils.SettingsUtil
 
 class SettingsActivity : AppCompatActivity() {
     lateinit var app: MyApplication
 
-    private val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+    private val binding: ActivitySettingsBinding by lazy {
+        ActivitySettingsBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,10 +21,21 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(binding.root)
         app = application as MyApplication
 
+        binding.permissionsButton.setOnClickListener {
+            val intent = Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        binding.permissionsButton.isClickable = !SettingsUtil.isNotificationAccessEnabled(this)
     }
 }
