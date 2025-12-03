58b9a79f8d087bd9870aa1a1256945cf24bc401a
kuderluka <luka.kuder@gmail.com>
2025-12-03 17:25:57 +0100

    KAPUT-12: Pokaze se opozorilo, ce ni dovoljenja za dostop do obvestil

--- app/src/main/java/si/um/feri/kaput/MainActivity.kt
+++ app/src/main/java/si/um/feri/kaput/MainActivity.kt
@@ -1,10 +1,13 @@
 package si.um.feri.kaput
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import si.um.feri.kaput.databinding.ActivityMainBinding
+import si.um.feri.kaput.utils.SettingsUtil
 
 class MainActivity : AppCompatActivity() {
     lateinit var app: MyApplication
@@ -23,5 +26,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (!SettingsUtil.isNotificationAccessEnabled(this)) {
+            showPermissionRequestDialog()
+        }
+    }
+
+    private fun showPermissionRequestDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Additional Permissions Required")
+            .setMessage("This app requires notification access to function properly. Please enable it in the settings.")
+            .setPositiveButton("Go to Settings") { _, _ ->
+                startActivity(Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"))
+            }
+            .setNegativeButton("Cancel", null)
+            .show()
     }
 }
