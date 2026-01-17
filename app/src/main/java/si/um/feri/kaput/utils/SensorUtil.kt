8c18b4192138b50c3a97ee0e8a7aacacb3c81462
kuderluka <luka.kuder@gmail.com>
2026-01-17 23:32:38 +0100

    KAPUT-57: Dodan senzor in spremenjen nacin uploadanja

--- app/src/main/java/si/um/feri/kaput/utils/SensorUtil.kt
+++ app/src/main/java/si/um/feri/kaput/utils/SensorUtil.kt
@@ -1,6 +1,10 @@
 package si.um.feri.kaput.utils
 
 import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -11,10 +15,25 @@ object SensorUtil {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var location: SimpleLocation
 
+    private var sensorManager: SensorManager? = null
+    private var pressure: Float? = null
+
     fun init(context: Context) {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
         location = SimpleLocation(context)
         location.beginUpdates()
+
+        sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        val barometer = sensorManager?.getDefaultSensor(Sensor.TYPE_PRESSURE)
+        barometer?.let {
+            sensorManager?.registerListener(object : SensorEventListener {
+                override fun onSensorChanged(event: SensorEvent) {
+                    pressure = event.values[0]
+                }
+                override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+            }, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
     }
 
     fun getLocation(): Map<String, Any> {
@@ -24,6 +43,10 @@ object SensorUtil {
         data["longitude"] = location.getLongitude()
 
         return data
+    }
+
+    fun getPressure(): Float? {
+        return pressure
     }
 
     fun stopLocationUpdates() {
